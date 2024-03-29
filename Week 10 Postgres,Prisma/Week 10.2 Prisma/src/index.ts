92b18c213854b9717a5c8c5d372a7a6f51395699
Ali Murtaza <syedalimurtaza36@gmail.com>
2024-03-30 03:13:37 +0500

    prisma compelete

--- Week 10 Postgres,Prisma/Week 10.2 Prisma/src/index.ts
+++ Week 10 Postgres,Prisma/Week 10.2 Prisma/src/index.ts
@@ -1,5 +1,42 @@
 import { PrismaClient } from "@prisma/client";
 const prismaClient=new PrismaClient();
 
-prismaClient.users.create({});
-prismaClient.todos.create({});
+async function insertInto(
+  username: string,
+  password: string,
+  firstname: string,
+  lastname: string
+) {
+  const res = await prismaClient.users.create({
+    data: {
+      username: username,
+      password: password,
+      firstName: firstname,
+      lastName: lastname,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
+  console.log(res);
+}
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {
+  prismaClient.users.update({
+    where: {
+      username,
+    },
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+    },
+  });
+}
